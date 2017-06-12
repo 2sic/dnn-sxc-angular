ebe8898deaaea226cc8fdeb2330a3a3fae72960a
2SIC\2cb <christoph.buehler@2sic.com>
2017-06-12 11:17:15 +0200

    added Http providers to export

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { SxcAngularModule } from './sxc-angular.module';
 export { ContentResourceFactory } from './sxc-content.service';
 export { DnnAngular, SxcAngular } from './sxc-angular.service';
 export { DnnAppComponent, SxcAppComponent } from './sxc-app.component';
+export { DnnHttpProvider, SxcHttpProvider } from './sxc-http.provider';
